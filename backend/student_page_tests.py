d9f039db7e1d6eaaa107bfaa9d17c110ef4d2f87
snnus <kurt.pichev@mail.ru>
2024-03-25 01:11:28 +0500

    add more tests and small fixes

--- backend/student_page_tests.py
+++ backend/student_page_tests.py
@@ -35,10 +35,11 @@ class StudentPageTests(unittest.TestCase):
         response = self.loop.run_until_complete(
             self.get_request(url="/api/cum_sum_points_for_stud_for_team", **params))
         self.assertEqual(response['status'], 200)
+        # todo: жалуется на mimetype
 
         for item in response['response_json']:
             self.assertIsInstance(item["name"], str)
-            self.assertIsInstance(item["cum_sum"], int)
+            self.assertIsInstance(item["cum_sum"], float)
             self.assertIsInstance(item["counter"], int)
             self.assertIsInstance(item["isTest"], bool)
             self.assertIsNotNone(item["name"])
@@ -47,7 +48,7 @@ class StudentPageTests(unittest.TestCase):
             self.assertIsNotNone(item["isTest"])
 
 
-    def test_attendance_dynamical_for_stud_for_team(self):
+    def test_attendance_dynamical_for_stud_for_team_true(self):
         params = {'id_stud': 144,
                   'id_team': 2}
         response = self.loop.run_until_complete(
@@ -56,11 +57,20 @@ class StudentPageTests(unittest.TestCase):
 
         for item in response['response_json']:
             self.assertIsInstance(item["name"], str)
-            self.assertIsInstance(item["dynamical_arrival"], int)
+            self.assertIsInstance(item["dynamical_arrival"], float)
             self.assertIsNotNone(item["name"])
             self.assertIsNotNone(item["dynamical_arrival"])
 
-    def test_attendance_static_for_stud_for_team(self):
+
+    def test_attendance_dynamical_for_stud_for_team_false(self):
+        params = {'id_stud': None,
+                  'id_team': None}
+        response = self.loop.run_until_complete(
+            self.get_request(url="/api/attendance_dynamical_for_stud_for_team", **params))
+        self.assertEqual(response['status'], 422)
+
+
+    def test_attendance_static_for_stud_for_team_true(self):
         params = {'id_stud': 144,
                   'id_team': 2}
         response = self.loop.run_until_complete(
@@ -73,6 +83,13 @@ class StudentPageTests(unittest.TestCase):
             self.assertIsNotNone(item["name"])
             self.assertIsNotNone(item["static_arrival"])
 
+    def test_attendance_static_for_stud_for_team_false(self):
+        params = {'id_stud': None,
+                  'id_team': None}
+        response = self.loop.run_until_complete(
+            self.get_request(url="/api/attendance_static_for_stud_for_team", **params))
+        self.assertEqual(response['status'], 422)
+
     def test_all_in_one_for_stud_for_team(self):
         params = {'id_stud': 144,
                   'id_team': 2}
@@ -83,8 +100,8 @@ class StudentPageTests(unittest.TestCase):
         for item in response['response_json']:
             self.assertIsInstance(item["name"], str)
             self.assertIsInstance(item["static_arrival"], float)
-            self.assertIsInstance(item["dynamical_arrival"], int)
-            self.assertIsInstance(item["cum_sum"], int)
+            self.assertIsInstance(item["dynamical_arrival"], float)
+            self.assertIsInstance(item["cum_sum"], float)
             self.assertIsInstance(item["counter"], int)
             self.assertIsInstance(item["isTest"], bool)
             self.assertIsNotNone(item["name"])
@@ -93,6 +110,13 @@ class StudentPageTests(unittest.TestCase):
             self.assertIsNotNone(item["cum_sum"])
             self.assertIsNotNone(item["counter"])
             self.assertIsNotNone(item["isTest"])
+
+    def test_all_in_one_for_stud_for_team(self):
+        params = {'id_stud': None,
+                  'id_team': None}
+        response = self.loop.run_until_complete(
+            self.get_request(url="/api/all_in_one_for_stud_for_team", **params))
+        self.assertEqual(response['status'], 422)
 
 
 
