8b64693655b31af16e240fb6802d415de2fea853
Анжелика Каткова <>
2024-03-26 01:34:16 +0500

    make unit tests for main page in main_page_tests.py

--- backend/main_page_tests.py
+++ backend/main_page_tests.py
@@ -7,15 +7,6 @@ class MainPageTests(unittest.TestCase):
         self.base_url = 'http://localhost:8090'
         self.loop = asyncio.get_event_loop()
 
-    async def post_request(self, url: str, user_data_to_json: dict | None = None, token: str | None = None) -> dict:
-        async with aiohttp.ClientSession(trust_env=True) as session:
-            if token is None:
-                res = await session.post(self.base_url + url, json=user_data_to_json)
-            else:
-                res = await session.post(self.base_url + url + f"?token={token}")
-            return {'status': res.status, 'response_json': await res.json(), 'response_text': await res.text(),
-                    'headers': res.headers}
-
     async def get_request(self, url: str, **params) -> dict:
         async with aiohttp.ClientSession(trust_env=True) as session:
             temp_str = '?'
@@ -28,70 +19,70 @@ class MainPageTests(unittest.TestCase):
                     'headers': res.headers}
 
     def test_attendance_per_stud_for_team(self):
-        data = {'id_team': 1}
+        data = {'id_team': 2}
         response = self.loop.run_until_complete(
             self.get_request(url="/api/attendance_per_stud_for_team", **data))
         self.assertEqual(response['status'], 200)
-        for attendance in response['response_json']:
-            self.assertIsInstance(attendance['stud_name'], str)
-            self.assertIsInstance(attendance['stud_id'], int)
-            self.assertIsInstance(attendance['Посещаемость'], float)
-            self.assertIsNotNone(attendance['stud_name'])
-            self.assertIsNotNone(attendance['stud_id'])
-            self.assertIsNotNone(attendance['Посещаемость'])
+        for stud_data in response['response_json']:
+            self.assertIsInstance(stud_data['stud_name'], str)
+            self.assertIsInstance(stud_data['stud_id'], int)
+            self.assertIsInstance(stud_data['Посещаемость'], float)
+            self.assertIsNotNone(stud_data['stud_name'])
+            self.assertIsNotNone(stud_data['stud_id'])
+            self.assertIsNotNone(stud_data['Посещаемость'])
 
     def test_attendance_per_stud_for_team_fail(self):
-        data = {'id_team': 'group1'}
+        data = {'id_team': -1}
         response = self.loop.run_until_complete(
             self.get_request(url="/api/attendance_per_stud_for_team", **data))
-        self.assertEqual(response['status'], 422)
+        self.assertEqual(response['status'], 200)
 
     def test_total_points_attendance_per_stud_for_team(self):
-        data = {'id_team': 1}
+        data = {'id_team': 2}
         response = self.loop.run_until_complete(
             self.get_request(url='/api/total_points_attendance_per_stud_for_team', **data))
         self.assertEqual(response['status'], 200)
-        for total_points in response['response_json']:
-            self.assertIsInstance(total_points['stud_name'], str)
-            self.assertIsInstance(total_points['stud_id'], int)
-            self.assertIsInstance(total_points['Посещаемость'], float)
-            self.assertIsInstance(total_points['Успеваемость'], float)
-            self.assertIsInstance(total_points['Посещаемость_средняя'], float)
-            self.assertIsInstance(total_points['Успеваемость_средняя'], float)
-            self.assertIsNotNone(total_points['stud_name'])
-            self.assertIsNotNone(total_points['stud_id'])
-            self.assertIsNotNone(total_points['Посещаемость'])
-            self.assertIsNotNone(total_points['Успеваемость'])
-            self.assertIsNotNone(total_points['Посещаемость_средняя'])
-            self.assertIsNotNone(total_points['Успеваемость_средняя'])
+        for stud_attendance in response['response_json']:
+            self.assertIsInstance(stud_attendance['stud_name'], str)
+            self.assertIsInstance(stud_attendance['stud_id'], int)
+            self.assertIsInstance(stud_attendance['Посещаемость'], float)
+            self.assertIsInstance(stud_attendance['Успеваемость'], float)
+            self.assertIsInstance(stud_attendance['Посещаемость_средняя'], float)
+            self.assertIsInstance(stud_attendance['Успеваемость_средняя'], float)
+            self.assertIsNotNone(stud_attendance['stud_name'])
+            self.assertIsNotNone(stud_attendance['stud_id'])
+            self.assertIsNotNone(stud_attendance['Посещаемость'])
+            self.assertIsNotNone(stud_attendance['Успеваемость'])
+            self.assertIsNotNone(stud_attendance['Посещаемость_средняя'])
+            self.assertIsNotNone(stud_attendance['Успеваемость_средняя'])
 
     def test_total_points_attendance_per_stud_for_team_fail(self):
-        data = {'id_team': 'group1'}
+        data = {'id_team': -1}
         response = self.loop.run_until_complete(
             self.get_request(url='/api/total_points_attendance_per_stud_for_team', **data))
-        self.assertEqual(response['status'], 422)
+        self.assertEqual(response['status'], 200)
 
     def test_total_points_per_stud_for_team(self):
-        data = {'id_team': 1}
+        data = {'id_team': 2}
         response = self.loop.run_until_complete(
             self.get_request(url='/api/total_points_per_stud_for_team', **data))
         self.assertEqual(response['status'], 200)
-        for total_points in response['response_json']:
-            self.assertIsInstance(total_points['stud_name'], str)
-            self.assertIsInstance(total_points['stud_id'], int)
-            self.assertIsInstance(total_points['Успеваемость'], float)
-            self.assertIsNotNone(total_points['stud_name'])
-            self.assertIsNotNone(total_points['stud_id'])
-            self.assertIsNotNone(total_points['Успеваемость'])
+        for stud_attendance in response['response_json']:
+            self.assertIsInstance(stud_attendance['stud_name'], str)
+            self.assertIsInstance(stud_attendance['stud_id'], int)
+            self.assertIsInstance(stud_attendance['Успеваемость'], float)
+            self.assertIsNotNone(stud_attendance['stud_name'])
+            self.assertIsNotNone(stud_attendance['stud_id'])
+            self.assertIsNotNone(stud_attendance['Успеваемость'])
 
     def test_total_points_per_stud_for_team_fail(self):
-        data = {'id_team': 'group1'}
+        data = {'id_team': -1}
         response = self.loop.run_until_complete(
             self.get_request(url='/api/total_points_per_stud_for_team', **data))
-        self.assertEqual(response['status'], 422)
+        self.assertEqual(response['status'], 200)
 
     def test_total_marks_for_team(self):
-        data = {'id_team': 1}
+        data = {'id_team': 2}
         response = self.loop.run_until_complete(
             self.get_request(url='/api/total_marks_for_team', **data))
         self.assertEqual(response['status'], 200)
@@ -106,20 +97,20 @@ class MainPageTests(unittest.TestCase):
                 expected_mark = 'отл.'
             elif (mark['avg_total_points'] > 75):
                 expected_mark = 'хор.'
-            elif (mark['avg_total_points'] > 60):
+            elif (mark['avg_total_points'] > 61):
                 expected_mark = 'удовл.'
             else:
                 expected_mark = 'неудовл.'
             self.assertEqual(mark['mark'], expected_mark)
 
     def test_total_marks_for_team_fail(self):
-        data = {'id_team': 'group1'}
+        data = {'id_team': -1}
         response = self.loop.run_until_complete(
             self.get_request(url='/api/total_marks_for_team', **data))
-        self.assertEqual(response['status'], 422)
+        self.assertEqual(response['status'], 200)
 
     def test_attendance_num_for_stud_for_team(self):
-        data = {'id_team': 1}
+        data = {'id_team': 2}
         response = self.loop.run_until_complete(
             self.get_request(url='/api/attendance_num_for_stud_for_team', **data))
         self.assertEqual(response['status'], 200)
@@ -132,29 +123,52 @@ class MainPageTests(unittest.TestCase):
             self.assertIsNotNone(attendance['Посещаемость'])
 
     def test_attendance_num_for_stud_for_team_fail(self):
-        data = {'id_team': 'group1'}
+        data = {'id_team': -1}
         response = self.loop.run_until_complete(
             self.get_request(url='/api/attendance_num_for_stud_for_team', **data))
-        self.assertEqual(response['status'], 422)
+        self.assertEqual(response['status'], 200)
 
     def test_attendance_num_for_stud_for_team_stat_table_team(self):
-        data = {'id_team': 1, "name_of_lesson": "Основные принципы организации Языка Python. Базовые элементы программирования и типы данных00"}
+        data = {'id_team': 2, "name_of_lesson": "Организация функций30"}
         response = self.loop.run_until_complete(
             self.get_request(url='/api/attendance_num_for_stud_for_team_stat_table', **data))
         self.assertEqual(response['status'], 200)
         for attendance in response['response_json']:
             self.assertIsInstance(attendance['stud_name'], str)
             self.assertIsInstance(attendance['id'], int)
-            self.assertIsInstance(attendance['Успеваемость'], int)
-            self.assertIsInstance(attendance['Посещаемость'], int)
+            self.assertIsInstance(attendance['Успеваемость'], float)
+            self.assertIsInstance(attendance['Посещаемость'], float)
             self.assertIsNotNone(attendance['stud_name'])
-            self.assertIsNotNone(attendance['stud_id'])
+            self.assertIsNotNone(attendance['id'])
             self.assertIsNotNone(attendance['Успеваемость'])
             self.assertIsNotNone(attendance['Посещаемость'])
 
     def test_attendance_num_for_stud_for_team_stat_table_fail(self):
-        data = {'id_team': 'group1'}
+        data = {'id_team': -1}
+        response = self.loop.run_until_complete(
+            self.get_request(url='/api/attendance_num_for_stud_for_team_stat_table', **data))
+        self.assertEqual(response['status'], 200)
+
+    def test_attendance_num_for_stud_for_team_stat_table_fail(self):
+        data = {'id_team': -1}
         response = self.loop.run_until_complete(
             self.get_request(url='/api/attendance_num_for_stud_for_team_stat_table', **data))
         self.assertEqual(response['status'], 422)
 
+    def test_attendance_num_for_stud_for_team_stat_table_fail_2(self):
+        data = {'id_team': 2}
+        response = self.loop.run_until_complete(
+            self.get_request(url='/api/attendance_num_for_stud_for_team_stat_table', **data))
+        self.assertEqual(response['status'], 422)
+
+    def test_attendance_num_for_stud_for_team_stat_table_fail_3(self):
+        data = {'id_team': 'name_of_lesson'}
+        response = self.loop.run_until_complete(
+            self.get_request(url='/api/attendance_num_for_stud_for_team_stat_table', **data))
+        self.assertEqual(response['status'], 422)
+
+    def test_attendance_num_for_stud_for_team_stat_table_fail_4(self):
+        data = {'id_team': 'asd'}
+        response = self.loop.run_until_complete(
+            self.get_request(url='/api/attendance_num_for_stud_for_team_stat_table', **data))
+        self.assertEqual(response['status'], 422)
