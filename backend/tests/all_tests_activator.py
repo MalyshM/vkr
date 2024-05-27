8b2182e5c7c9fb29e5ad3ba94a99add0344e0c36
Малыш Михаил Сергеевич <>
2024-05-27 12:19:33 +0500

    create `all_tests_activator.py`

---
+++ backend/tests/all_tests_activator.py
@@ -0,0 +1,14 @@
+import unittest
+
+from routers.group_comparison_page.group_comparison_page_tests import GroupComparisonPageTests
+from routers.main_page.main_page_tests import MainPageTests
+from routers.registration_page.registration_tests import RegistrationTests
+from routers.speciality_comparison_page.speciality_comparison_page_tests import SpecialityComparisonTests
+from routers.student.student_tests import StudentTests
+from routers.student_page.student_page_tests import StudentPageTests
+from routers.team.team_tests import TeamTests
+from routers.user.user_tests import UserTests
+from routers.util.util_tests import UtilTests
+
+if __name__ == '__main__':
+    unittest.main()
